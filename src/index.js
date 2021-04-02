13d24ab086a9bd66c86db3f018a67f099fbbe23f
chanayus <chanayus1526@hotmail.com>
2021-04-02 08:45:41 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// This is your Editor pane. Write your JavaScript here and 
-// use the command line to execute commands
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Lab 9 Done !!'))
 
 app.listen(port, "0.0.0.0", () => console.log(`Web app listening on port ${port}!`))
